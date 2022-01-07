bd3f78931f41ab96de6f5a1bb5d70885ce130042
Alireza Fereydoni <Aliirzw@gmail.com>
2022-01-08 02:54:52 +0330

    Get products

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div className="App"></div>;
-}
+import { useQuery } from "react-query";
+
+// types
+import { iProduct } from "./types/interfaces";
+
+// components
+import { LinearProgress } from "@mui/material";
+
+const getProducts = async () => await (await fetch("https://fakestoreapi.com/products")).json();
+
+const App: React.FC = () => {
+  const { data, isLoading, error } = useQuery<iProduct[]>("products", getProducts);
+
+  console.log(data);
+
+  if (isLoading) return <LinearProgress />;
+  if (error) return <div>something went wrong...</div>;
+
+  return <div className="App">hello</div>;
+};
 
 export default App;
