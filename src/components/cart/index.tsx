2ea299147467cd489a5f800a51c00fc98727719f
Alireza Fereydoni <Aliirzw@gmail.com>
2022-01-08 03:34:31 +0330

    Implementing cart item

---
+++ src/components/cart/index.tsx
@@ -0,0 +1,28 @@
+// type
+import { Button } from "@mui/material";
+import { iProduct } from "../../types/interfaces";
+
+// style
+import { CardWrapper } from "./style";
+
+interface iCard {
+  item: iProduct;
+  add: (clickedItem: iProduct) => void;
+}
+
+const CartItem: React.FC<iCard> = ({ item, add }) => {
+  return (
+    <CardWrapper>
+      <img src={item.image} alt={item.title} />
+      <div>
+        <h3>{item.title}</h3>
+        <p>{item.description}</p>
+        <h3>${item.price}</h3>
+      </div>
+
+      <Button onClick={() => add(item)}>Add to cart</Button>
+    </CardWrapper>
+  );
+};
+
+export { CartItem };
